063c08453f4df64d8b38db274261df296417ed75
Prateek <prateek.sharma03@nagarro.com>
2024-02-18 17:23:56 +0530

    Add db connection properties

--- Data/CatalogContext.cs
+++ Data/CatalogContext.cs
@@ -8,8 +8,10 @@ namespace Catalog.API.Data
     public class CatalogContext : ICatalogContext
     {
         public CatalogContext(IConfiguration configuration)
-        {            
-            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        {
+            var mongoConnectionString = $"mongodb://{configuration.GetValue<string>("DatabaseSettings:MongoDbUser")}:{configuration.GetValue<string>("DatabaseSettings:MongoDbPassword")}" +
+                $"@{configuration.GetValue<string>("DatabaseSettings:MongoDbHost")}:{configuration.GetValue<string>("DatabaseSettings:MongoDbPort")}";
+            var client = new MongoClient(mongoConnectionString);
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
